41ed38c909c017581f7f2dc5211dd6699e9bb8c0
Titouan Galopin <titouan@selency.com>
2024-01-21 21:10:43 +0100

    WIP

--- tests/Context/EventFactoryTest.php
+++ tests/Context/EventFactoryTest.php
@@ -18,41 +18,6 @@ class EventFactoryTest extends UnitTest
 {
     public function provideCreateEvent()
     {
-        $packages = array(
-            'deprecationsio/monolog-handler' => 'dev-main',
-            'doctrine/instantiator' => '2.0.0.0',
-            'monolog/monolog' => '3.5.0.0',
-            'myclabs/deep-copy' => '1.11.1.0',
-            'nikic/php-parser' => '5.0.0.0',
-            'phar-io/manifest' => '2.0.3.0',
-            'phar-io/version' => '3.2.1.0',
-            'phpunit/php-code-coverage' => '9.2.30.0',
-            'phpunit/php-file-iterator' => '3.0.6.0',
-            'phpunit/php-invoker' => '3.1.1.0',
-            'phpunit/php-text-template' => '2.0.4.0',
-            'phpunit/php-timer' => '5.0.3.0',
-            'phpunit/phpunit' => '9.6.16.0',
-            'psr/log' => '3.0.0.0',
-            'psr/log-implementation' => 'dev-main',
-            'sebastian/cli-parser' => '1.0.1.0',
-            'sebastian/code-unit' => '1.0.8.0',
-            'sebastian/code-unit-reverse-lookup' => '2.0.3.0',
-            'sebastian/comparator' => '4.0.8.0',
-            'sebastian/complexity' => '2.0.3.0',
-            'sebastian/diff' => '4.0.5.0',
-            'sebastian/environment' => '5.1.5.0',
-            'sebastian/exporter' => '4.0.5.0',
-            'sebastian/global-state' => '5.0.6.0',
-            'sebastian/lines-of-code' => '1.0.4.0',
-            'sebastian/object-enumerator' => '4.0.4.0',
-            'sebastian/object-reflector' => '2.0.4.0',
-            'sebastian/recursion-context' => '4.0.5.0',
-            'sebastian/resource-operations' => '3.0.3.0',
-            'sebastian/type' => '3.2.1.0',
-            'sebastian/version' => '3.0.2.0',
-            'theseer/tokenizer' => '1.2.2.0',
-        );
-
         return array(
             array(
                 'sapi' => 'cli',
@@ -60,8 +25,8 @@ class EventFactoryTest extends UnitTest
                     'argv' => array('bin/console', 'cache:clear'),
                 ),
                 'expectedProjectDir' => dirname(dirname(__DIR__)),
-                'expectedPayload' => array(
-                    'packages' => $packages,
+                'expectedPayloadKeys' => array('packages', 'deprecations', 'command'),
+                'expectedPayloadData' => array(
                     'deprecations' => array(),
                     'command' => 'bin/console cache:clear',
                 ),
@@ -72,8 +37,8 @@ class EventFactoryTest extends UnitTest
                     'REQUEST_URI' => 'https://example.com/path/to/url?query=param#hash',
                 ),
                 'expectedProjectDir' => dirname(dirname(__DIR__)),
-                'expectedPayload' => array(
-                    'packages' => $packages,
+                'expectedPayloadKeys' => array('packages', 'deprecations', 'method', 'url'),
+                'expectedPayloadData' => array(
                     'deprecations' => array(),
                     'method' => 'GET',
                     'url' => '/path/to/url?query=param',
@@ -86,8 +51,8 @@ class EventFactoryTest extends UnitTest
                     'REQUEST_URI' => '/path/to/url?query=param#hash',
                 ),
                 'expectedProjectDir' => dirname(dirname(__DIR__)),
-                'expectedPayload' => array(
-                    'packages' => $packages,
+                'expectedPayloadKeys' => array('packages', 'deprecations', 'method', 'url'),
+                'expectedPayloadData' => array(
                     'deprecations' => array(),
                     'method' => 'POST',
                     'url' => '/path/to/url?query=param',
@@ -99,7 +64,7 @@ class EventFactoryTest extends UnitTest
     /**
      * @dataProvider provideCreateEvent
      */
-    public function testCreateEvent($sapi, $server, $expectedProjectDir, $expectedPayload)
+    public function testCreateEvent($sapi, $server, $expectedProjectDir, $expectedPayloadKeys, $expectedPayloadData)
     {
         $_SERVER = array_merge($_SERVER, $server);
 
@@ -108,7 +73,13 @@ class EventFactoryTest extends UnitTest
 
         $this->assertInstanceOf('Deprecationsio\Monolog\Context\Event', $event);
         $this->assertSame($expectedProjectDir, $event->getProjectDir());
-        $this->assertSame($expectedPayload, $event->toArray());
+
+        $payload = $event->toArray();
+        $this->assertSame($expectedPayloadKeys, array_keys($payload));
+
+        foreach ($expectedPayloadData as $key => $expected) {
+            $this->assertSame($expected, $payload[$key]);
+        }
     }
 
     public function testAddDeprecation()
@@ -140,6 +111,6 @@ class EventFactoryTest extends UnitTest
         $this->assertSame(27, $details['deprecations'][0]['line']);
 
         $traceLines = explode("\n", $details['deprecations'][0]['trace']);
-        $this->assertSame('tests/Context/EventFactoryTest.php	125	Tests\\Deprecationsio\\Monolog\\UnitTest::createDeprecationException', $traceLines[0]);
+        $this->assertSame('tests/Context/EventFactoryTest.php	96	Tests\\Deprecationsio\\Monolog\\UnitTest::createDeprecationException', $traceLines[0]);
     }
 }
